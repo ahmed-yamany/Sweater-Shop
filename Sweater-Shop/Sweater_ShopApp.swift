8927775b1cf40091600af78cf252bfb7e7f3117a
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2022-12-08 17:42:21 +0200

    Initial Commit

---
+++ Sweater-Shop/Sweater_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sweater_ShopApp.swift
+//  Sweater-Shop
+//
+//  Created by Ahmed Yamany on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Sweater_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
